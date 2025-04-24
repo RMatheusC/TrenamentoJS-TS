033429aa820f7ea335078e3ee49c50f6862697fe
Matheus Caccia Ribeiro <matheusc.r@hotmail.com>
2025-04-23 21:35:21 -0400

    Ajuste dotenv e operação com MongoDB
    Ajustado o problema com o dotenv e realizado a conexão com o banco de dados.

--- Archives/Modules/express.js
+++ Archives/Modules/express.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const UserModel = require('../Modules/src/Models/user.model');
 const app = express();
+
+app.use(express.json());
 const port = 8080;
 
 app.get('/home', (req, res) => {
@@ -7,17 +10,23 @@ app.get('/home', (req, res) => {
     res.status(200).send("<h1>Hello World!</h1>");
 });
 
-app.get('/users', (req, res) => {
-    const users = [
-        {
-            name: 'John',
-            email: 'J@teste.com'
-        },
-        {
-            name: 'Mark',
-            email:'M@teste.com'   
-        }]
-        res.status(200).json(users);
+app.get('/users', async (req, res) => {
+    try {
+       const users = await UserModel.find({});
+       
+       res.status(200).send(error.message);
+    } catch (error) {
+       return res.status(500).send(error.message);
+    }
+});
+
+app.post('/users', async (req, res)=> {
+    try {
+        const user = await UserModel.create(req.body);
+        res.status(201).json(user);     
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 app.listen(port, () => console.log(`Rodando com Express na porta ${port}`));
--- Archives/index.js
+++ Archives/index.js
@@ -1,19 +1,6 @@
-// require("./Modules/path");
-// require("./Modules/fs");
-// require("./Modules/http");
-// require("./Modules/express");
 const {Person} = require("./person");
-// const dotenv = require('dotenv');
-const dotenv = require('dotenv'); //verificar erro - não está respeitando o .env
-dotenv.config();
+const dotenv = require('dotenv');
+const express = require('./Modules/express');
 const connectToDatabase = require('./Modules/src/database/connect');
-
-
-// console.log('teste:', process.env.MONGODB_USERNAME);
-
 dotenv.config();
-
 connectToDatabase();
-
-
-// const person = new Person("Matheus");
